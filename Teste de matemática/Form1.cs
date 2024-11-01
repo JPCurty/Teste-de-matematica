10f58c9d2f40f7a6d3676de0a2af87596659e2c2
Aula <Aula@CANP-LAB7-C01>
2024-11-01 11:32:18 -0300

    Adicionar arquivos de projeto.

---
+++ Teste de matemática/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teste_de_matemática
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public partial class Form1 : Form
+        {
+            // Create a Random object called randomizer 
+            // to generate random numbers.
+            Random randomizer = new Random();
+
+            // These integer variables store the numbers 
+            // for the addition problem. 
+            int addend1;
+            int addend2;
+
+            // These integer variables store the numbers 
+            // for the subtraction problem. 
+            int minuend;
+            int subtrahend;
+
+            // These integer variables store the numbers 
+            // for the multiplication problem. 
+            int multiplicand;
+            int multiplier;
+
+            // These integer variables store the numbers 
+            // for the division problem. 
+            int dividend;
+            int divisor;
+            }
+
+            private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
